824135879a1ba4eb4be7931c93241377966a7e8c
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2022-04-06 14:04:15 +0000

    Label nome.

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
   </head>
   <body>
     <h1> Cadastrar Uurário </h1> 
+    <form method = "POST" action = "processa.php">
+      <label> Nome: </label>
+      <input type = "text" name = "nome" placeholder = "Digite o nome completo "><br><br>
+    </form>
   </body>
 </html>
